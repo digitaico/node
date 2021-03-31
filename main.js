6f0ac0119c36b1eb5ac49fdd1fb45d360c99141a
Jorge Eduardo Ardila <jeam@tuta.io>
2021-03-31 17:57:31 -0500

    Server upstart

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 'use strict';
 
+var http = require("http");
+
+http.createServer((request, response) => {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hola mundo jea node\n');
+}).listen(8081);
+
+console.log('Server running at localhost:8081');
